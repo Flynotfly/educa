b7d65dc88717254fb50ae1e6ddd71f177a6cc1fd
Flynotfly <Alteria2004@gmail.com>
2024-08-15 15:33:45 +0400

    Add Subject to admin panel

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Subject, Course, Module
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title', )}
+
