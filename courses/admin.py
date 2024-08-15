38126794583888e460dfec9d1a0faf8e47e9d8c1
Flynotfly <Alteria2004@gmail.com>
2024-08-15 15:36:00 +0400

    Add Course to admin panel

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import Subject, Course, Module
 
 
@@ -9,3 +8,15 @@ class SubjectAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug']
     prepopulated_fields = {'slug': ('title', )}
 
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title', )}
+    inlines = [ModuleInline]
