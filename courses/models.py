b743f2aa5c3a2920dce73c11051b4e3c4bec29a1
Flynotfly <Alteria2004@gmail.com>
2024-08-15 15:23:26 +0400

    Create Subject model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
