5b41b633bea95efd1ec8d48163328f3d6290ad1b
Flynotfly <Alteria2004@gmail.com>
2024-08-15 15:28:01 +0400

    Make Subject's slug uniq

--- courses/models.py
+++ courses/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
         ordering = ['title']
