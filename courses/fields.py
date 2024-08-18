4dae52e6cb2af43afdd07e94e5aa0b22b9ceb8ae
Flynotfly <Alteria2004@gmail.com>
2024-08-18 10:16:11 +0400

    Debug OrderField

--- courses/fields.py
+++ courses/fields.py
@@ -21,4 +21,4 @@ class OrderField(models.PositiveIntegerField):
             setattr(model_instance, self.attname, value)
             return value
         else:
-            super().pre_save(model_instance, add)
+            return super().pre_save(model_instance, add)
