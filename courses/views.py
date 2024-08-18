773fe48a1878fcc6b9d8bb254e568b35210e9ea3
Flynotfly <Alteria2004@gmail.com>
2024-08-18 09:53:05 +0400

    Add get func to ContentCreateUpdateView

--- courses/views.py
+++ courses/views.py
@@ -114,4 +114,12 @@ class ContentCreateUpdateView(TemplateResponseMixin, View):
         if id:
             self.obj = get_object_or_404(self.model, id=id, owner=request.user)
         return super().dispatch(request, module_id, model_name, id)
-    
+
+    def get(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return self.render_to_response(
+            {
+                'form': form,
+                'object': self.obj,
+            }
+        )
