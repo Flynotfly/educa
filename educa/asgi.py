bca974e75a8c7e1f4d9777a93becaa5bfe983518
Flynotfly <Alteria2004@gmail.com>
2024-08-23 22:42:09 +0400

    Move import websocket_urlpatterns bellow get_asgi_application()

--- educa/asgi.py
+++ educa/asgi.py
@@ -14,11 +14,12 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 
 from django.core.asgi import get_asgi_application
 
-from chat.routing import websocket_urlpatterns
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
 
 django_asgi_app = get_asgi_application()
+
+# import websocket_urlpatterns after get_asgi_application() !!!
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
