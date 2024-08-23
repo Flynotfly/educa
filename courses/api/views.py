5c788ead717a1db43c0c914bc4a62d94d8959e8b
Flynotfly <Alteria2004@gmail.com>
2024-08-23 17:59:24 +0400

    Remove unused api views

--- courses/api/views.py
+++ courses/api/views.py
@@ -14,15 +14,15 @@ from courses.api.pagination import StandardPagination
 from courses.models import Course, Subject
 
 
-class SubjectListView(generics.ListAPIView):
-    queryset = Subject.objects.annotate(total_courses=Count('courses'))
-    serializer_class = SubjectSerializer
-    pagination_class = StandardPagination
-
-
-class SubjectDetailView(generics.RetrieveAPIView):
-    queryset = Subject.objects.annotate(total_courses=Count('courses'))
-    serializer_class = SubjectSerializer
+# class SubjectListView(generics.ListAPIView):
+#     queryset = Subject.objects.annotate(total_courses=Count('courses'))
+#     serializer_class = SubjectSerializer
+#     pagination_class = StandardPagination
+#
+#
+# class SubjectDetailView(generics.RetrieveAPIView):
+#     queryset = Subject.objects.annotate(total_courses=Count('courses'))
+#     serializer_class = SubjectSerializer
 
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
