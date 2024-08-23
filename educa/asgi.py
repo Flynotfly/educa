f638b44381b8bfc6e151cc3dc7f46493514d2b41
Flynotfly <Alteria2004@gmail.com>
2024-08-23 22:39:24 +0400

    Add websocket_urlpatterns to asgi application settings

--- educa/asgi.py
+++ educa/asgi.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 from django.core.asgi import get_asgi_application
+
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
 
@@ -19,4 +22,7 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
 })
