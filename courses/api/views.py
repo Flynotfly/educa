46c57ebdd0bae28f8f2ad8327b620e1ae0ebfe3f
Flynotfly <Alteria2004@gmail.com>
2024-08-23 17:50:02 +0400

    Add authentication to CourseEnrollView

--- courses/api/views.py
+++ courses/api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -35,6 +36,8 @@ class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
