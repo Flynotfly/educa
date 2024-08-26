a6b079798acf5d46410aadbdae63af2b130c1c44
Flynotfly <Alteria2004@gmail.com>
2024-08-26 11:50:49 +0400

    Add exit from channel group during disconect

--- chat/consumers.py
+++ chat/consumers.py
@@ -13,7 +13,7 @@ class ChatConsumer(WebsocketConsumer):
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
