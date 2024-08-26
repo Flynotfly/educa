966adce8ab1bcc2ac2aaa77a1f923b8b1e618b7c
Flynotfly <Alteria2004@gmail.com>
2024-08-26 11:54:58 +0400

    Send message to group when server receive message

--- chat/consumers.py
+++ chat/consumers.py
@@ -18,4 +18,7 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        self.send(text_data=json.dumps({'message': message}))
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {'type': 'chat_message', 'message': message}
+        )
